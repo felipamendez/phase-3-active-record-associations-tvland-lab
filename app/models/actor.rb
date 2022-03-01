c52d84ee0b65521e11d5896d56010ac10af4e018
Felipa Mendez <felipalouise@gmail.com>
2022-03-01 16:19:54 -0600

    commit tree a4d98bfd062c35921c145f3beed778cbe714dca6

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        name_array = self.characters.map {|c| "#{c.name} - #{c.show.name}" }
+        # shows_array = self.shows.map {|s| s.name}
+        # name_array  shows_array
+    end
+
+
 end
+
